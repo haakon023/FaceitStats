23e020e7819d3dbabc0b6a4acdb1cb2668ba2f5b
Håkon <haakonfj@gmail.com>
2024-05-05 18:03:53 +0200

    changed help command

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -1,17 +1,17 @@
-const { SlashCommandBuilder,EmbedBuilder } = require('discord.js')
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
 
 
 module.exports = {
     category: 'utility',
     data: new SlashCommandBuilder()
-    .setName("fshelp")
+    .setName("help")
     .setDescription("Get alls commands for Faceit Stats bot, and how to use them"),
     async execute (interaction){
         var message = new EmbedBuilder()
         .setTitle("Help for Faceit Stats commands")
-        .addFields("/stats playername", "Get the stats of a player. Playername is case sensetive. Optional extra parameter to add based on how many matches")
-        .addFields("/elo playername", "Get the current elo of a player. Playername is case sensetive.")
-        .addFields("/compare playername playername", "Compare two players, the player names are case sensetive.")
+        .addFields({name:"/stats playername", value:"Get the stats of a player. Playername is case sensetive. Optional extra parameter to add based on how many matches"})
+        .addFields({name:"/elo playername", value:"Get the current elo of a player. Playername is case sensetive."})
+        .addFields({name:"/compare playername playername", value:"Compare two players, the player names are case sensetive."})
         
         interaction.reply({embeds: [message]})
     }
--- deploy-commands.js
+++ deploy-commands.js
@@ -36,7 +36,7 @@ const rest = new REST().setToken(token);
 	try {
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-		// The put method is used to fully refresh all commands in the guild with the current set
+		
 		await rest.put(
             Routes.applicationCommands(clientId),
             { body: commands },
